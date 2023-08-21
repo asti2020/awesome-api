9555eb52d4a2365c91164fda842f20ac9ba72664
Astika Luintel <astild2020@gmail.com>
2023-08-21 19:44:33 -0400

    tokenuc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,37 @@
 class UsersController < ApplicationController
     protect_from_forgery
+    skip_before_action :authorized, only: [:create ]
+    rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid_record
 
 
     def create
-            user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
-            render json: {user: UserSerializer.new(user)}, status: :created
+            user = User.create(user_params)
+            @token = encode_token(user_id: user.id)
+            render json: {
+                user: UserSerializer.new(user),
+                token: @token
+            }, status: :created
     end
 
     def index
         @user = User.all
         render json: @user, status: :ok
     end
+
+    def me
+        render json: current_user, status: :ok
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :email, :password)
+    end
+
+    def handle_invalid_record(e)
+        render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+
+    
 end
